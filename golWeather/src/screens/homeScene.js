8a3cd040ab6a8ff126b86ce1e8fe9bda690ffa68
RenatoMartinsFerreira <linkrenato11@gmail.com>
2020-03-09 03:06:13 -0300

    Removing center container

--- golWeather/src/screens/homeScene.js
+++ golWeather/src/screens/homeScene.js
@@ -31,7 +31,6 @@ class HomeScene extends Component {
           <View style={styles.topContainer}>
             <Text style={{backgroundColor: 'white'}}>SearchBar</Text>
           </View>
-          <View style={styles.centerContainer} />
           <View style={styles.bottomContainer}>
             <View style={styles.visibleBottomContainer}>
               <Text>Bottom Content</Text>
@@ -68,9 +67,7 @@ const styles = StyleSheet.create({
     flex: 0.2,
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  centerContainer: {
-    flex: 0.5,
+    backgroundColor: 'red',
   },
   bottomContainer: {
     backgroundColor: 'white',
